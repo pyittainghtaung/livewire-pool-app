32d5e07083440db746dde0bee51917f222fd7c2a
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-04-18 12:13:13 +0630

    Voting in Polls

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Option;
 use Livewire\Component;
 
 class Polls extends Component
@@ -14,5 +15,10 @@ class Polls extends Component
         $polls = \App\Models\Poll::with('options.votes')->latest()->get();
 
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote(Option $option){
+        // $option=\App\Models\Option::findOrFail($optionId);
+        $option->votes()->create();
     }
 }
--- resources/views/livewire/polls.blade.php
+++ resources/views/livewire/polls.blade.php
@@ -4,7 +4,7 @@
             <h3>{{ $poll->title }}</h3>
             @foreach ($poll->options as $option)
                 <div class="mb-2">
-                    <button class="btn">Vote</button>
+                    <button class="btn" wire:click="vote({{ $option->id }})">Vote</button>
                     {{ $option->name }} ({{ $option->votes->count() }})
                 </div>
             @endforeach
